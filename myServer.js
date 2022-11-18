ef97888725cbe80aa8afb19bf6db7ab47101da0a
Jason Christopher <jchristopher2448@gmail.com>
2022-11-18 17:18:46 -0600

    finished Lab 10

---
+++ myServer.js
@@ -0,0 +1,33 @@
+'use strict';
+
+// REQUIRE
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const getWeather = require('./myWeather.js');
+const getMovies= require('./myMovies.js');
+
+// USE
+const app = express();
+app.use(cors());
+const PORT = process.env.PORT || 3002;
+
+// ROUTES
+app.get('/', (req, res) => {
+  res.status(200).send('Base Page');
+});
+app.get('/weather', getWeather);
+app.get('/movies', getMovies);
+app.get('*', (req, res) => {
+  res.status(404).send('That route does not exist');
+});
+
+// ERRORS
+app.use((error, req, res, next) => {
+  res.status(500).send(error.message);
+});
+
+// CLASSES
+
+// LISTEN
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
